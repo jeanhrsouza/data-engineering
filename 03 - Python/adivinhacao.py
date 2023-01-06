ab9d15d351d8cb4e8cbd278601722fd9e8e3b68b
Jean Souza <jeanhrsouza@hotmail.com>
2023-01-05 21:52:52 -0300

    feat: python - aula 03: testando valores

---
+++ 03 - Python/adivinhacao.py
@@ -0,0 +1,24 @@
+print("*" * 35)
+print("Bem vindo no jogo de Adivinhação!")
+print("*" * 35)
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+print("Você digitou: ", chute_str)
+chute = int(chute_str)
+
+acertou = numero_secreto == chute
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if (acertou):
+    print("Você acertou")
+else:
+    if(maior):
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor que o número secreto.")
+
+
+print("Fim do jogo!")
